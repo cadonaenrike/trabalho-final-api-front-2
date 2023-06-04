0a056b9cc097d7b0359c3d634f9a060661a17c2f
Everton cadona <cadonaenrike@gmail.com>
2023-06-04 10:14:55 -0300

    organizei o arquivo js e coloquei o windowonload pra cima

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,3 +1,5 @@
+window.onload = function() { contadorRodape(); };
+
 const listaPersonagens = document.getElementById("lista-personagens");
 const paginacao = document.getElementById("paginacao");
 const inputBusca = document.getElementById("input-busca");
@@ -6,11 +8,6 @@ const urlPersonagem = "https://rickandmortyapi.com/api/character";
 const urlepisodio = "https://rickandmortyapi.com/api/episode";
 const urlLocalizacao = "https://rickandmortyapi.com/api/location";
 
-
-
-window.onload = function() {
-  contadorRodape();
-};
 
 let paginaAtual = 1;
 let valorTraduzido = "";
@@ -49,25 +46,29 @@ function buscarPersonagens(pagina, nome = "") {
 
         const descricaolocalizacao = document.createElement("span")
         descricaolocalizacao.classList.add("descricaoLocalizacao");
+
         descricaolocalizacao.textContent = "Última localização conhecida" 
 
         const ultimaLocalizacao = document.createElement("p");
         ultimaLocalizacao.classList.add("personagem-ultimaLocalizacao");
+
         ultimaLocalizacao.textContent = personagem.location.name;
 
         const descricaoEpfinal = document.createElement("span")
         descricaoEpfinal.classList.add("descricaoEpfinal");
+
         descricaoEpfinal.textContent = "Visto última vez em:" 
 
         const ultimoEpisodio = document.createElement("p");
         ultimoEpisodio.classList.add("personagem-ultimoEpisodio");
-        const teste = await axios.get(personagem.episode[0]);
-        ultimoEpisodio.textContent = teste.data.name;
-        
 
+        const nomeDoEp = await axios.get(personagem.episode[0]);
+        ultimoEpisodio.textContent = nomeDoEp.data.name;
+        
 
         const divpaiinfo = document.createElement("div");
         divpaiinfo.classList.add("personagem-info");
+
         divpaiinfo.appendChild(nome);
         divpaiinfo.appendChild(status);
         divpaiinfo.appendChild(descricaolocalizacao);
@@ -75,13 +76,10 @@ function buscarPersonagens(pagina, nome = "") {
         divpaiinfo.appendChild(descricaoEpfinal);
         divpaiinfo.appendChild(ultimoEpisodio);
 
-
         itemLista.appendChild(imagem);
         itemLista.appendChild(divpaiinfo);
-      
-        
+              
         listaPersonagens.appendChild(itemLista);
-        console.log(itemLista)
       });
       
       paginacao.innerHTML = "";
@@ -89,7 +87,9 @@ function buscarPersonagens(pagina, nome = "") {
       if (informacoes.prev) {
         const botaoAnterior = document.createElement("button");
         botaoAnterior.classList.add("botaoProximo");
+
         botaoAnterior.textContent = "Anterior";
+
         botaoAnterior.addEventListener("click", function() {
           buscarPersonagens(informacoes.prev.split("=")[1], nome);
         });
@@ -99,7 +99,9 @@ function buscarPersonagens(pagina, nome = "") {
       if (informacoes.next) {
         const botaoProximo = document.createElement("button");
         botaoProximo.classList.add("botaoProximo");
-        botaoProximo.textContent = "Próximo";
+
+        botaoProximo.textContent = "Próximo"
+        ;
         botaoProximo.addEventListener("click", function() {
           buscarPersonagens(informacoes.next.split("=")[1], nome);
         });
@@ -116,18 +118,21 @@ function contadorRodape(){
   axios.get(urlPersonagem).then((res)=>{
     const quantidadePersonagens = res.data.info.count;
     const quantidadePersonagensFooter = document.getElementById("quantidadePersonagens");
+
     quantidadePersonagensFooter.textContent = "PERSONAGENS: " + quantidadePersonagens;
   })
 
   axios.get(urlLocalizacao).then((res)=>{
     const localizacao = res.data.info.count;
     const localizacaoPersonagensFooter = document.getElementById("localizacao");
+
     localizacaoPersonagensFooter.textContent = "LOCALIZAÇÕES: " + localizacao;
   })
 
   axios.get(urlepisodio).then((res)=>{
     const episodio = res.data.info.count;
     const quantidadePersonagensFooter = document.getElementById("episodio");
+    
     quantidadePersonagensFooter.textContent = "EPISÓDIOS: " + episodio;
   })
 
